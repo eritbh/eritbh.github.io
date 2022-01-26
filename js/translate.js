928ed55a488be3d32c27f0958d62f8792cb1728d
Erin <erin20913@gmail.com>
2022-01-25 22:44:12 -0500

    right, mutationobservers work like this

--- js/translate.js
+++ js/translate.js
@@ -1,13 +1,15 @@
 // detect google translate lang direction classes and adapt accordingly
-let observer = new MutationObserver(mutation => {
-	if (mutation.attributeName !== 'class') {
-		return;
-	}
+let observer = new MutationObserver(mutations => {
+	for (const mutation of mutations) {
+		if (mutation.attributeName !== 'class') {
+			return;
+		}
 
-	if (mutation.target.classList.contains('translated-rtl')) {
-		document.documentElement.dir = 'rtl';
-	} else if (mutation.target.classList.contains('translated-ltr')) {
-		document.documentElement.dir = 'ltr';
+		if (mutation.target.classList.contains('translated-rtl')) {
+			document.documentElement.dir = 'rtl';
+		} else if (mutation.target.classList.contains('translated-ltr')) {
+			document.documentElement.dir = 'ltr';
+		}
 	}
 });
 observer.observe(document.documentElement, {attributes: true});
