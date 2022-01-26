9ee1f3b6a64822b9d22b558137b73e99f34acf48
Erin <erin20913@gmail.com>
2022-01-25 22:35:27 -0500

    add some fancy google translate handling

---
+++ js/translate.js
@@ -0,0 +1,24 @@
+// detect google translate lang direction classes and adapt accordingly
+let observer = new MutationObserver(mutation => {
+	if (mutation.attributeName !== 'class') {
+		return;
+	}
+
+	if (mutation.target.classList.contains('translated-rtl')) {
+		document.documentElement.dir = 'rtl';
+	} else if (mutation.target.classList.contains('translated-ltr')) {
+		document.documentElement.dir = 'ltr';
+	}
+});
+observer.observe(document.documentElement, {attributes: true});
+
+// add dir=ltr to all pre and code elements since those need to stay literal
+document.addEventListener('readystatechange', () => {
+	if (document.readyState !== 'complete') {
+		return;
+	}
+
+	for (const el of document.querySelectorAll('pre, code')) {
+		el.dir = 'ltr';
+	}
+});
