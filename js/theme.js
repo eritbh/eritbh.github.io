0978934e708f5c00b0a8464833a111ba71c72233
Erin <erin20913@gmail.com>
2022-09-18 21:23:22 -0400

    simplify everything what was i doing

--- js/theme.js
+++ js/theme.js
@@ -3,48 +3,14 @@ const LOCALSTORAGE_KEY = 'theme'
 const getBrowserTheme = () => window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 const getStoredTheme = () => localStorage.getItem(LOCALSTORAGE_KEY);
 
-let themeToggleButton = document.querySelector('.theme-button');
-let themeResetButton = null;
-
-document.addEventListener('readystatechange', () => {
-	if (document.readyState !== 'complete') {
-		return;
-	}
-
-	themeToggleButton = document.querySelector('.theme-button');
-	updatePageTheme();
-})
-
-function createThemeResetButton () {
-	if (themeResetButton || !themeToggleButton) {
-		return;
-	}
-	themeResetButton = document.createElement('button');
-	themeResetButton.classList.add('theme-button', 'theme-reset-button');
-	themeResetButton.setAttribute('title', 'Reset to browser/system theme');
-	themeResetButton.setAttribute('aria-label', 'Reset to browser/system theme');
-	themeResetButton.addEventListener('click', () => {
-		resetTheme();
-	});
-	themeToggleButton.after(themeResetButton);
-}
-
-function removeThemeResetButton () {
-	if (!themeResetButton) {
-		return;
-	}
-	themeResetButton.remove();
-	themeResetButton = null;
-}
-
 // set theme based on previous state or OS/browser setting
 function updatePageTheme () {
 	const storedTheme = getStoredTheme();
 	document.documentElement.setAttribute('data-theme', storedTheme || getBrowserTheme());
-	if (storedTheme != null) {
-		createThemeResetButton();
+	if (storedTheme) {
+		document.documentElement.setAttribute('data-local-theme', '');
 	} else {
-		removeThemeResetButton();
+		document.documentElement.removeAttribute('data-local-theme');
 	}
 }
 
